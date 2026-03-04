e2ff868554c490726a473dda95f74fdc0e699f6a
David Maniliuc <david.maniliuc@gmail.com>
2026-03-04 16:40:31 +0100

    feat: add project structure

---
+++ gb-core/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod cpu;
+pub mod memory;
+pub mod registers;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
