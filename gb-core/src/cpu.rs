2b7651fe94b9216c5ec44cce212bb2ffc1aaa3ec
David Maniliuc <david.maniliuc@gmail.com>
2026-03-10 17:24:29 +0100

    feat: 0xF6 opcode

--- gb-core/src/cpu.rs
+++ gb-core/src/cpu.rs
@@ -614,6 +614,11 @@ impl Cpu {
                 self.push(bus, AddrSource::AF);
                 16
             }
+            0xF6 /* OR A, n8 */ => {
+                let n8 = Self::fetch_u8(bus, &mut self.pc);
+                self.or_u8(Reg8::A, n8);
+                8
+            }
 
             v @ (0xD3 | 0xDB | 0xDD | 0xE3 | 0xE4 | 0xEB | 0xEC | 0xED | 0xF4 | 0xFC | 0xFD) => {
                 panic!("Illegal opcode {:#04X} encountered", v);
--- gb-core/tests/cpu_tests.rs
+++ gb-core/tests/cpu_tests.rs
@@ -513,6 +513,18 @@ macro_rules! test_or {
             assert_eq!(t, $cycles);
         }
     };
+
+    ($(#[$attr:meta])* r8_n8, $name:ident, $opcode:expr, $val_a:expr, $imm:expr, $expected:expr, $z:expr, $cycles:expr) => {
+        $(#[$attr])* #[test]
+        fn $name() {
+            let (mut cpu, mut bus) = setup_test!(&[$opcode, $imm]);
+            cpu.registers.a = $val_a;
+            let t = cpu.step(&mut bus);
+            assert_eq!(cpu.registers.a, $expected);
+            assert_flags!(cpu, $z, false, false, false);
+            assert_eq!(t, $cycles);
+        }
+    };
 }
 
 macro_rules! test_cp {
